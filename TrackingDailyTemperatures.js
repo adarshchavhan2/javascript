b4d6ba49a5b2071fd30d72c97ac088efcf7599fa
adarshchavhan2 <adarshchavhan2@gmail.com>
2025-04-21 18:22:23 +0530

    Create TrackingDailyTemperatures.js
    L0 - Array Manipulation: Tracking Daily Temperatures
    
    Problem Statement:
    Create an array that stores the temperatures recorded over 7 days. Use indexing to access the temperature for the 4th day and update it to a new value by adding 2 degrees. After that, log the updated temperatures for all 7 days to the console.

---
+++ TrackingDailyTemperatures.js
@@ -0,0 +1,9 @@
+/*
+Create an array that stores the temperatures recorded over 7 days. Use indexing to access the temperature for the 4th day and update it to a new value by adding 2 degrees. After that, log the updated temperatures for all 7 days to the console.
+*/
+
+let temperatures = [34,35, 36, 38, 39, 36, 42];
+
+temperatures[3]+=2;
+
+console.log(" updated temperatures for all 7 days: "+temperatures);
